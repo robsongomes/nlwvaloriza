84ec9a802b5c08442b252327bc2b65415aeaffd0
Robson Gomes <nosbor84@gmail.com>
2021-06-25 12:28:30 -0300

    adiçao do cors

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import "reflect-metadata";
 import express, { Request, Response, NextFunction } from "express"; 
 import "express-async-errors";
+import cors from "cors"
 
 import { router } from "./routes";
 
@@ -9,6 +10,8 @@ import "./database"
 const PORT = process.env.PORT || 3001
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
